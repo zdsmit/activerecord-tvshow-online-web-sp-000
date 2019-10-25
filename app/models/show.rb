f60988f109225fc2a721cf3afcb82bb0b2a4a681
Zach  Smith <zdsmith001@gmail.com>
2019-10-25 22:09:52 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,18 +9,23 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
+    Show.minimum(:rating)
   end
 
   def self.least_popular_show
+    Show.where("rating = ?", lowest_rating).first
   end
 
   def self.ratings_sum
+    Show.sum(:rating)
   end
 
   def self.popular_shows
+    Show.where("rating > 5")
   end
 
   def self.shows_by_alphabetical_order
+    Show.order(:name)
   end
 
 end
